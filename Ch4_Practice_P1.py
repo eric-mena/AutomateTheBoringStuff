f828d793071c894b80afcc64d0685660ca130b7e
eric-mena <59487263+eric-mena@users.noreply.github.com>
2020-04-05 15:23:56 -0700

    Update Ch4_Practice_P1.py

--- Ch4_Practice_P1.py
+++ Ch4_Practice_P1.py
@@ -1,1 +1,26 @@
+# CommaCode.py
 
+# Say you have a list value like this:
+# spam = ['apples', 'bananas', 'tofu', 'cats']
+# Write a function that takes a list value as an argument and returns
+# a string with all the items separated by a comma and a space, with and
+# inserted before the last item. For example, passing the previous spam list to
+# the function would return 'apples, bananas, tofu, and cats'. But your function 
+# should be able to work with any list value passed to it.
+
+def fun(aList):
+    for i in aList:
+        if aList.index(i) < len(aList) - 1:
+            print(i + ', ', end='')
+        else:
+            print('and ' + i + '.')
+
+inputs = []
+while True:
+    print('Enter a string or number. Enter a blank to stop.')
+    response = input()
+    if response == '':
+        break
+    elif response != '':
+        inputs.append(response)
+fun(inputs)
