2c1bd01a3b6c22eb2153b3cc99bcf24705f87d91
eric-mena <59487263+eric-mena@users.noreply.github.com>
2020-04-05 16:11:09 -0700

    Update Ch4_Practice_P1.py

--- Ch4_Practice_P1.py
+++ Ch4_Practice_P1.py
@@ -4,6 +4,33 @@
 # spam = ['apples', 'bananas', 'tofu', 'cats']
 # Write a function that takes a list value as an argument and returns
 # a string with all the items separated by a comma and a space, with and
+# inserted before the last item. For example, passing the previous spam 
+# list to the function would return 'apples, bananas, tofu, and cats'. 
+# But your function should be able to work with any list value passed to it.
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+cats = ['Fuzzy', 'Furry', 'Fluffy', 'Funky']
+cheeses = ['gouda', 'muenster']
+one = [1, 2, 3]
+
+def edit(listToEdit):
+    for i in listToEdit:
+        if listToEdit.index(i) < len(listToEdit) - 1:
+            print(str(i) + ', ', end='')
+        else:
+            print('and ' + str(i) + '.')
+
+edit(spam)
+edit(cats)
+edit(cheeses)
+edit(one)
+
+# CommaCodeAlt.py
+
+# Say you have a list value like this:
+# spam = ['apples', 'bananas', 'tofu', 'cats']
+# Write a function that takes a list value as an argument and returns
+# a string with all the items separated by a comma and a space, with and
 # inserted before the last item. For example, passing the previous spam list to
 # the function would return 'apples, bananas, tofu, and cats'. But your function 
 # should be able to work with any list value passed to it.
